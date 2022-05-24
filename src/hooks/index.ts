284439eb466417da8db2f24f72b3f7059481bfbe
kensoz <kensozlian@gmail.com>
2022-05-24 16:32:53 +0900

    fix bug

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -7,7 +7,7 @@
 export default function useHooks() {
   // 背景色作成
   const bgColorCreator = (e: number): string => {
-    // デフォルト、本社、支社、食堂、外勤、その他
+    // 0:デフォルト、1:本社、2:支社、3:食堂、4:外勤、5:その他
     const list: string[] = ['bg-slate-100', 'bg-blue-100', 'bg-green-100', 'bg-red-100', 'bg-violet-100', 'bg-zinc-100']
     return list[e]
   }
