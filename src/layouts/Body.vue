cf2df0cd1becddbce77e9b2b423a35204e38829e
kensoz <kensozlian@gmail.com>
2022-07-02 17:05:27 +0900

    add admin btn

--- src/layouts/Body.vue
+++ src/layouts/Body.vue
@@ -56,8 +56,28 @@
     </template>
   </section>
 
-  <!-- go to topボタン -->
+  <!-- vueテレポート -->
   <teleport to="body">
+    <!-- Adminボタン -->
+    <div class="fixed bottom-24 right-6">
+      <button
+        class="shadow-sm hover:shadow bg-blue-200 hover:bg-blue-300 p-2 rounded-full"
+        type="button"
+        @click="jump()"
+      >
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+            <path
+              fill-rule="evenodd"
+              d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+      </button>
+    </div>
+
+    <!-- go to topボタン -->
     <div class="fixed bottom-10 right-6">
       <button
         class="shadow-sm hover:shadow bg-yellow-200 hover:bg-yellow-300 p-2 rounded-full"
@@ -93,6 +113,12 @@
       top: 0,
       behavior: 'smooth',
     })
+  }
+
+  // ----- リンク -----
+  const GitHub = ''
+  const jump = (): void => {
+    window.location.href = GitHub
   }
 
   // ダークモード
